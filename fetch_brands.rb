f3ce507ef2a6c6d3758c786a526fbe00eec4da92
free1 <747549945@qq.com>
2015-07-27 13:10:28 +0800

    抓取网页商品品牌

---
+++ fetch_brands.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+require 'active_record'
+
+
+class Brand < ActiveRecord::Base
+
+	db_config = {
+		:adapter  => "mysql2",
+	  :encoding => 'utf8',
+	  # :host     => "54.223.213.136",
+	  :username => "root",
+	  :password => "123",
+	  :database => "makehave_development"
+	}
+
+	establish_connection(db_config)
+end
+
+puts "---begin---"
+
+html = open('http://www.farfetch.com/cn/designers-women.aspx?ffref=hd_mnav').read
+doc = Nokogiri::HTML(html)
+
+doc.css('#designerGroupWrapper li > a').map do |href|
+	puts href.text.strip
+	Brand.create(name: href.text.strip)
+end
