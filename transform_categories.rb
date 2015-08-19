95870890dd1fa4dfcf8903d3ef70225b4dbf0d17
free <free@makehave.com>
2015-08-19 11:06:22 +0800

    update

---
+++ transform_categories.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# require 'active_record'
+
+# class Product < ActiveRecord::Base
+
+# 	db1_config = {
+# 	:adapter  => "mysql2",
+# 	  :encoding => 'utf8',
+# 	  :host     => "54.223.188.18",
+# 	  :username => "root",
+# 	  :password => "password",
+# 	  :database => "makehave"
+# 	}
+
+# 	establish_connection(db1_config)
+# end
+
+# class Category < ActiveRecord::Base
+
+# 	db1_config = {
+# 		:adapter  => "mysql2",
+# 	  :encoding => 'utf8',
+# 	  :host     => "localhost",
+# 	  :username => "root",
+# 	  :password => "123",
+# 	  :database => "makehave_development"
+# 	}
+
+# 	establish_connection(db1_config)
+# end
+
+# Category.find_by_name("women")
